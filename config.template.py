ced6dd995e54d8403dfb618cf532b8044f33cceb
BugMakerH <1371444344@qq.com>
2024-04-27 17:18:30 +0100

    update config.template.py

--- config.template.py
+++ config.template.py
@@ -1,22 +1,42 @@
+import models
+
 config_wandb = {
-    'api_key': '',  # Add your API key here
+    'api_key': '',  # Your W&B API key
     'project': 'test',
 }
 
+config_train = {
+    'model': models.ImprovedCNN,
+    'optimizer': 'Adam',
+    'epochs': 100,
+    'lr': 0.001,
+    'weight_decay': 0.001,
+
+    'fig_path': './fig',
+}
+
 config_cifar100 = {
-    'train': {
-        'batch_size': 128,
-        'shuffle': True,
-        'num_workers': 8,
-    },
-    'val': {
-        'batch_size': 128,
-        'shuffle': False,
-        'num_workers': 8,
-    },
-    'test': {
-        'batch_size': 128,
-        'shuffle': False,
-        'num_workers': 8,
+    'root': './tmp_dataset',
+    'datasets': {
+        'train': {
+            'batch_size': 128,
+            'shuffle': True,
+            'num_workers': 8,
+            'augmentation': {
+                'flip': True,
+                'crop': True,
+            },
+        },
+        'val': {
+            'ratio': 0.2,  # 20% of the training dataset will be used for validation
+            'batch_size': 128,
+            'shuffle': False,
+            'num_workers': 8,
+        },
+        'test': {
+            'batch_size': 128,
+            'shuffle': False,
+            'num_workers': 8,
+        },
     },
 }
