38fef9bd17ae31f921290e2b72fe2206377af4f1
BugMakerH <1371444344@qq.com>
2024-04-28 17:42:17 +0100

    Update config.template.py

--- config.template.py
+++ config.template.py
@@ -10,11 +10,11 @@ WANDB_ENTITY = 'bugmakerh'  # Your entity name. You can find it in the URL of yo
 default_program_config = {
     'production': True,  # [True, False]. Set this to 'True' if you want to run the code in production mode. But it must be 'True' when you run the code in sweep mode.
     'mode': 'sweep',    # ['train', 'sweep']. 'train' is for training a single model, and 'sweep' is for hyperparameter tuning.
-    'model': 'EnhancedCNN',  # ['SimpleCNN', 'EnhancedCNN', 'ImprovedCNN', 'InceptionLikeCNN'].
+    'model': 'ResidualCNN',  # ['SimpleCNN', 'EnhancedCNN', 'ImprovedCNN', 'InceptionLikeCNN', 'ResidualCNN'].
     'dataset': 'CIFAR100',  # ['CIFAR100']
     'wandb': {
         'api_key': WANDB_API_KEY,
-        'project': 'ObjectionRecognition',   # Your project name in wandb
+        'project': 'Test',   # Your project name in wandb
         'entity': WANDB_ENTITY,
     },
 }
@@ -44,6 +44,12 @@ default_train_config = {
     },
     'ImprovedCNN': {
         'dropout_rate': 0.5,    # Dropout rate
+        'activation_function': 'LeakyReLU', # Activation function to use. For more activation functions, you can check the 'torch.nn' module from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity.
+    },
+    'InceptionLikeCNN': {
+        'activation_function': 'LeakyReLU', # Activation function to use. For more activation functions, you can check the 'torch.nn' module from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity.
+    },
+    'ResidualCNN': {
         'activation_function': 'LeakyReLU', # Activation function to use. For more activation functions, you can check the 'torch.nn' module from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity.
     },
 }
@@ -80,7 +86,7 @@ default_dataset_config = {
 
 # Default sweep configurations
 default_sweep_config = {
-    'count': 500,     # Number of runs
+    'count': 1,     # Number of runs
     'config': {
         # Model-specific configurations
         'SimpleCNN': {
@@ -153,7 +159,7 @@ default_sweep_config = {
                     'values': [64, 128, 256]
                 },
                 'epochs': {
-                    'value': 3
+                    'value': 200
                 },
                 'optimizer': {
                     'values': ['Adam', 'SGD']   # For more optimizers, you can check the 'torch.optim' module from https://pytorch.org/docs/stable/optim.html#algorithms.
@@ -165,6 +171,56 @@ default_sweep_config = {
                     'min': 0.0,
                     'max': 0.5
                 }
+            }
+        },
+        'InceptionLikeCNN': {
+            'method': 'bayes',
+            'metric': {
+                'name': 'val_accuracy',
+                'goal': 'maximize'
+            },
+            'parameters': {
+                'learning_rate': {
+                    'min': 1e-4,
+                    'max': 1e-2
+                },
+                'batch_size': {
+                    'values': [64, 128, 256]
+                },
+                'epochs': {
+                    'value': 200
+                },
+                'optimizer': {
+                    'values': ['Adam', 'SGD']   # For more optimizers, you can check the 'torch.optim' module from https://pytorch.org/docs/stable/optim.html#algorithms.
+                },
+                'activation_function': {
+                    'values': ['ReLU', 'LeakyReLU']   # For more activation functions, you can check the 'torch.nn' module from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity.
+                },
+            }
+        },
+        'ResidualCNN': {
+            'method': 'bayes',
+            'metric': {
+                'name': 'val_accuracy',
+                'goal': 'maximize'
+            },
+            'parameters': {
+                'learning_rate': {
+                    'min': 1e-4,
+                    'max': 1e-2
+                },
+                'batch_size': {
+                    'values': [64, 128, 256]
+                },
+                'epochs': {
+                    'value': 1
+                },
+                'optimizer': {
+                    'values': ['Adam', 'SGD']   # For more optimizers, you can check the 'torch.optim' module from https://pytorch.org/docs/stable/optim.html#algorithms.
+                },
+                'activation_function': {
+                    'values': ['ReLU', 'LeakyReLU']   # For more activation functions, you can check the 'torch.nn' module from https://pytorch.org/docs/stable/nn.html#non-linear-activations-weighted-sum-nonlinearity.
+                },
             }
         }
     }
