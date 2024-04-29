f26e35c5b53d63e1f0727b2fc10dce58d9937679
BugMakerH <1371444344@qq.com>
2024-04-29 11:50:35 +0100

    add SimpleAlexNet.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from .EnhancedCNN import EnhancedCNN
 from .ImprovedCNN import ImprovedCNN
 from .InceptionLikeCNN import InceptionLikeCNN
 from .ResidualCNN import ResidualCNN
+from .SimpleAlexNet import SimpleAlexNet
